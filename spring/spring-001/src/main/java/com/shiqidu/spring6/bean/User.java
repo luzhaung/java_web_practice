e1d26699fa735441cf5c37933a4a687eeb3c8de2
luzhuang <401301965@qq.com>
2023-08-29 00:18:38 +0800

    spring6 first

--- spring/spring-001/src/main/java/com/shiqidu/spring6/bean/User.java
+++ spring/spring-001/src/main/java/com/shiqidu/spring6/bean/User.java
@@ -1,4 +1,7 @@
 package com.shiqidu.spring6.bean;
 
 public class User {
+    public User() {
+        System.out.println("User无参数构造方法执行");
+    }
 }
--- spring/spring-001/src/test/java/com/shiqidu/spring6/test/FirstSpringTest.java
+++ spring/spring-001/src/test/java/com/shiqidu/spring6/test/FirstSpringTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class FirstSpringTest {
 
     @Test
-    public void testFirstSpringCode()
-    {
+    public void testFirstSpringCode() {
         // spring容器
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         Object userBean = applicationContext.getBean("userBean");
@@ -16,5 +23,21 @@ public class FirstSpringTest {
 
         Object userDaoBean = applicationContext.getBean("userDaoBean");
         System.out.println(userDaoBean);
+    }
+
+    @Test
+    public void testHttpRequest() throws IOException {
+        String url = "https://clearplate-server-buff.oss-cn-beijing.aliyuncs.com/api/systemConf.json";
+        URL urlObj = new URL(url);
+        HttpURLConnection urlConnection = (HttpURLConnection) urlObj.openConnection();
+        urlConnection.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        System.out.println(response);
     }
 }
