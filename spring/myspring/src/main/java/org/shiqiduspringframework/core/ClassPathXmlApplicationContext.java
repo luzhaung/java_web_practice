116435364b1d2bbbf602df6c4aa1b7cc1fe5a966
luzhuang <401301965@qq.com>
2024-05-27 23:06:19 +0800

    myspring framework

--- spring/myspring/src/main/java/org/shiqiduspringframework/core/ClassPathXmlApplicationContext.java
+++ spring/myspring/src/main/java/org/shiqiduspringframework/core/ClassPathXmlApplicationContext.java
@@ -2,6 +2,8 @@ package org.shiqiduspringframework.core;
 
 import java.io.InputStream;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -52,6 +54,99 @@ public class ClassPathXmlApplicationContext implements ApplicationContext {
 
                     singletonObjects.put(id, bean);
                     logger.info(singletonObjects.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+
+            // 给对象赋值
+            nodes.forEach(node -> {
+                try {
+                    Element beanElement = (Element) node;
+                    String id = beanElement.attributeValue("id");
+                    String className = beanElement.attributeValue("class");
+                    Class<?> aClass = Class.forName(className);
+                    beanElement.elements("property").forEach(property -> {
+                        try {
+                            // 获取属性名
+                            String propertyName = property.attributeValue("name");
+                            logger.info("propertyName = " + propertyName);
+                            // 获取属性类型
+                            Field field = aClass.getDeclaredField(propertyName);
+                            // 获取set方法
+                            String setMethodName = "set" + propertyName.toUpperCase().charAt(0) + propertyName.substring(1);
+                            Method setMethod = aClass.getDeclaredMethod(setMethodName, field.getType());
+                            // 获取具体的值
+                            String value = property.attributeValue("value");
+                            Object actualValue = null;
+                            String ref = property.attributeValue("ref");
+                            if (value != null) {
+                                // 只支持 byte short int long float double boolean char
+                                String propertyTypeSimpleName = field.getType().getSimpleName();
+                                switch (propertyTypeSimpleName) {
+                                    case "byte":
+                                        actualValue = Byte.parseByte(value);
+                                        break;
+                                    case "short":
+                                        actualValue = Short.parseShort(value);
+                                        break;
+                                    case "int":
+                                        actualValue = Integer.parseInt(value);
+                                        break;
+                                    case "long":
+                                        actualValue = Long.parseLong(value);
+                                        break;
+                                    case "float":
+                                        actualValue = Float.parseFloat(value);
+                                        break;
+                                    case "double":
+                                        actualValue = Double.parseDouble(value);
+                                        break;
+                                    case "boolean":
+                                        actualValue = Boolean.parseBoolean(value);
+                                        break;
+                                    case "char":
+                                        actualValue = value.charAt(0);
+                                        break;
+                                    case "Byte":
+                                        actualValue = Byte.valueOf(value);
+                                        break;
+                                    case "Short":
+                                        actualValue = Short.valueOf(value);
+                                        break;
+                                    case "Integer":
+                                        actualValue = Integer.valueOf(value);
+                                        break;
+                                    case "Long":
+                                        actualValue = Long.valueOf(value);
+                                        break;
+                                    case "Float":
+                                        actualValue = Float.valueOf(value);
+                                        break;
+                                    case "Double":
+                                        actualValue = Double.valueOf(value);
+                                        break;
+                                    case "Boolean":
+                                        actualValue = Boolean.valueOf(value);
+                                        break;
+                                    case "Character":
+                                        actualValue = value.charAt(0);
+                                        break;
+                                    case "String":
+                                        actualValue = value;
+                                }
+                                // 调用方法
+                                setMethod.invoke(singletonObjects.get(id), actualValue);
+                            }
+                            if (ref != null) {
+                                // 调用方法
+                                setMethod.invoke(singletonObjects.get(id), singletonObjects.get(ref));
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    });
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
--- spring/myspring/src/test/java/com/shiqidu/myspring/test/MySpringTest.java
+++ spring/myspring/src/test/java/com/shiqidu/myspring/test/MySpringTest.java
@@ -1,5 +1,6 @@
 package com.shiqidu.myspring.test;
 
+import com.shiqiduuser.myspring.bean.UserService;
 import org.junit.Test;
 import org.shiqiduspringframework.core.ApplicationContext;
 import org.shiqiduspringframework.core.ClassPathXmlApplicationContext;
@@ -15,6 +16,10 @@ public class MySpringTest {
     @Test
     public void testMySpring() {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("myspring.xml");
+        Object user = applicationContext.getBean("user");
+        System.out.println(user);
+        UserService userService = (UserService) applicationContext.getBean("userService");
+        System.out.println(userService);
 
     }
 }
