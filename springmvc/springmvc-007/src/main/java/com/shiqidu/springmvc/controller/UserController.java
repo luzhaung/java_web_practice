8110d07343e5e221a60043f64665a93f41637550
luzhuang <401301965@qq.com>
2025-07-14 01:19:20 +0800

    spring mvc

--- springmvc/springmvc-007/src/main/java/com/shiqidu/springmvc/controller/UserController.java
+++ springmvc/springmvc-007/src/main/java/com/shiqidu/springmvc/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.shiqidu.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +17,13 @@ public class UserController {
 
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String getAll() {
-        System.out.println("正在查询所有用户信息...") ;
+        System.out.println("正在查询所有用户信息...");
+        return "ok";
+    }
+
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+    public String getUserById(@PathVariable("id") String id) {
+        System.out.println("正在根据用户地查询用户信息，用户Id是：" + id);
         return "ok";
     }
 }
