c3d8abb8884c0fa810e6dfbbcfdd595dda18ecd1
luzhuang <401301965@qq.com>
2025-12-25 23:25:11 +0800

    springboot-ssm

--- SpringBoot3/springboot-ssm/src/main/java/com/shiqidu/springboot/SpringbootSsmApplication.java
+++ SpringBoot3/springboot-ssm/src/main/java/com/shiqidu/springboot/SpringbootSsmApplication.java
@@ -1,15 +1,24 @@
 package com.shiqidu.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@Slf4j
 @MapperScan(basePackages = {"com.shiqidu.springboot.dao"})
 @SpringBootApplication
 public class SpringbootSsmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootSsmApplication.class, args);
+
+        log.trace("trace级别日志");
+        log.debug("debuge级别日志");
+        log.info("info级别日志");
+        log.warn("warn级别日志");
+        log.error("error级别日志");
     }
 
 }
